ec9266ac956455bf2fb0e9d3cda91f227d666b17
Tsoumenis Vangelis <vtsoumenis@codevise.de>
2025-12-02 18:20:35 +0100

    Adds test for initialization of Note from NoteType

--- test/xeno/content/changes/initialize_from_type_test.exs
+++ test/xeno/content/changes/initialize_from_type_test.exs
@@ -126,5 +126,79 @@ defmodule Xeno.Content.Changes.InitializeFromTypeTest do
       assert is_nil(note.data)
       assert is_nil(note.tags)
     end
+
+    test "does not copy empty initial_tags list", %{directory: dir} do
+      {:ok, empty_tags_type} =
+        NoteType.create(%{
+          name: "EmptyTags",
+          initial_text: "Some text",
+          initial_tags: []
+        })
+
+      {:ok, note} =
+        Note.create(%{
+          name: "Test",
+          directory_id: dir.id,
+          note_type_id: empty_tags_type.id
+        })
+
+      assert note.text == "Some text"
+      assert is_nil(note.tags) or note.tags == []
+    end
+
+    test "handles empty string initial_text as nil", %{directory: dir} do
+      {:ok, empty_text_type} =
+        NoteType.create(%{
+          name: "EmptyText",
+          initial_text: ""
+        })
+
+      {:ok, note} =
+        Note.create(%{
+          name: "Test",
+          directory_id: dir.id,
+          note_type_id: empty_text_type.id
+        })
+
+      assert is_nil(note.text)
+    end
+
+    test "copies empty map initial_data", %{directory: dir} do
+      {:ok, empty_data_type} =
+        NoteType.create(%{
+          name: "EmptyData",
+          initial_data: %{}
+        })
+
+      {:ok, note} =
+        Note.create(%{
+          name: "Test",
+          directory_id: dir.id,
+          note_type_id: empty_data_type.id
+        })
+
+      assert note.data == %{}
+    end
+
+    test "copies only non-nil template values", %{directory: dir} do
+      {:ok, partial_type} =
+        NoteType.create(%{
+          name: "Partial",
+          initial_text: "Has text",
+          initial_data: nil,
+          initial_tags: ["tag1"]
+        })
+
+      {:ok, note} =
+        Note.create(%{
+          name: "Test",
+          directory_id: dir.id,
+          note_type_id: partial_type.id
+        })
+
+      assert note.text == "Has text"
+      assert is_nil(note.data)
+      assert note.tags == ["tag1"]
+    end
   end
 end
